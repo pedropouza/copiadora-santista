0d53ae5af57d92ced7b4e725b8c94249143203e7
Pedro Pouza <124061818+pedropouza@users.noreply.github.com>
2023-09-26 20:11:04 +0100

    Update Input File
    Adicionando funcionalidade e estilo ao Input File

--- scripts/form-script.js
+++ scripts/form-script.js
@@ -1,5 +1,5 @@
+// Efeito do Input com Label
 const inputs = document.querySelectorAll('form p input, textarea, select')
-
 inputs.forEach(function(input) {
   input.addEventListener('blur', () => {
     if (input.value !== '') {
@@ -9,4 +9,29 @@ inputs.forEach(function(input) {
     }
   });
 })
+
+// Documentos com Botao de Limpar
+const inputFile = document.getElementById('attachImg')
+const fileList = document.getElementById('fileList')
+const resetListBtn = document.getElementById('resetListBtn')
+
+inputFile.addEventListener('change', () => {
+  const files = inputFile.files
+  for (let i=0; i < files.length; i++) {
+    const fileName = files[i].name
+    const newLi = document.createElement('li')
+    newLi.innerHTML = fileName
+    fileList.append(newLi)
+  }
+  resetListBtn.style.display = 'block'
+})
+
+resetListBtn.addEventListener('click', () => {
+  inputFile.value = ""
+  const allLi = document.querySelectorAll('#fileList li')
+  allLi.forEach(function(ev) {
+    ev.remove()
+  })
+  resetListBtn.style.display = 'none'
+}) 
 
